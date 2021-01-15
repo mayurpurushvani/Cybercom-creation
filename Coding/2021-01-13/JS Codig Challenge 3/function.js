905c81730c70cd378417e62e44dd5a16a06cf800
mayur <mayurpurushvani@gmai.com>
2021-01-15 11:48:13 +0530

    edited file

--- Coding/2021-01-13/JS Codig Challenge 3/function.js
+++ Coding/2021-01-13/JS Codig Challenge 3/function.js
@@ -1,75 +1,19 @@
-var restaurent1 = 124;
-var restaurent2 = 48;
-var restaurent3 = 268;
+var rest = [124, 48, 268];
+var r1Bill = [];
 
-function calculateTip()
-{
-    //Restaurent 1
-    if (restaurent1 <= 50)
-    {
-        var r1Bill = (restaurent1 * 20) / 100;
-        console.log("Total amount of restaurent 1 is : " + restaurent1)
-        console.log("Total amount of tip is :" +r1Bill);
+function calculateTip() {
+    for (i = 0; i < 3; i++) {
+        if (rest[i] <= 50) {
+            r1Bill[i] = (rest[i] * 20) / 100;
+        }
+        else if (rest[i] >= 50 && rest[i] <= 200) {
+            r1Bill[i] = (rest[i] * 15) / 100;
+        }
+        else {
+            r1Bill[i] = (rest[i] * 10) / 100;
+        }
     }
-    
-    else if(restaurent1 >= 50 && restaurent1 <= 200)
-    {
-        var r1Bill = (restaurent1 * 15) / 100;
-        console.log("Total amount of restaurent 1 is : " + restaurent1)
-        console.log("Total amount of tip is :" +r1Bill);
-    }
-
-    else
-    {
-        var r1Bill = (restaurent1 * 10) / 100;
-        console.log("Total amount of restaurent 1 is : " + restaurent1)
-        console.log("Total amount of tip is :" +r1Bill);
-    }
-
-    //Restaurent 2
-    if(restaurent2 <= 50)
-    {
-        var r2Bill = (restaurent2 * 20) / 100;
-        console.log("Total amount of restaurent 2 is : " + restaurent2)
-        console.log("Total amount of tip is :" +r2Bill);
-    }
-
-    else if(restaurent1 >= 50 && restaurent1 <= 200)
-    {
-        var r2Bill = (restaurent2 * 15) / 100;
-        console.log("Total amount of restaurent 2 is : " + restaurent2)
-        console.log("Total amount of tip is :" +r2Bill);
-    }
-
-    else
-    {
-        var r2Bill = (restaurent2 * 10) / 100;
-        console.log("Total amount of restaurent 2 is : " + restaurent2)
-        console.log("Total amount of tip is :" +r2Bill);
-    }
-    
-
-    //Restaurent 3
-    if(restaurent3 <= 50)
-    {   
-        var r3Bill = (restaurent3 * 20) / 100;
-        console.log("Total amount of restaurent 3 is : " + restaurent3)
-        console.log("Total amount of tip is : " +r3Bill);
-    }
-    else if(restaurent3 >= 50 && restaurent3 <= 200)
-    {
-        var r3Bill = (restaurent3 * 15) / 100;
-        console.log("Total amount of restaurent 3 is : " + restaurent3)
-        console.log("Total amount of tip is :" +r3Bill);
-    }
-
-    else
-    {
-        var r3Bill = (restaurent3 * 10) / 100;
-        console.log("Total amount of restaurent 3 is : " + restaurent3)
-        console.log("Total amount of tip is :" +r3Bill);
-    }
-    
-
-};
+    console.log("Total amount of restaurent is :"+rest);
+    console.log("<br>Total amount of tip is :"+r1Bill);
+ };
 calculateTip();
