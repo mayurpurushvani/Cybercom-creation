6f69db6fab0ab57c546a08ac13d3cae2270c8f7b
mayur <mayurpurushvani@gmai.com>
2021-01-15 20:27:09 +0530

    Const excercise added

---
+++ Coding/2021-01-15/Excercise/Const variable/script.js
@@ -0,0 +1,41 @@
+/*let a = { };
+a = {name : 'mayur',
+	age : 22 };
+
+console.log(a);
+
+var b = a;
+
+b.name = 'anjali';
+
+b.age = 23;
+
+console.log(b);
+
+console.log(a);
+
+*/
+
+
+/* 
+We can not change the values of const keyword in simple variables. but this will not work in the key value pair.
+It will change everytime when you can change any it. and also your assigned values change in front of.
+The above example is not work with const value but the below example works fine!
+*/
+
+
+const x = 50;
+
+console.log(x);
+
+y = x;
+
+console.log(y);
+
+y = 10;
+
+console.log(y);
+
+console.log(x);
+
+
