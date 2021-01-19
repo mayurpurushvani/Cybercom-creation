21de8fa29e9e1806fd03c2d7bc477c13f1589e3e
mayur <mayurpurushvani@gmai.com>
2021-01-19 21:38:22 +0530

    File Modified

--- Coding/2021-01-19/Excercise/HTMLFormExample/script.js
+++ Coding/2021-01-19/Excercise/HTMLFormExample/script.js
@@ -1,37 +1,14 @@
-var x = 0;
-var array = Array();
 
-function addElement()
-{
-    array[x] = document.getElementsByName("name").value;
-    array[x+1] = document.getElementsByName("email").value;
-    array[x+2] = document.getElementsByName("dateOfBirth").value;
-    alert('Element ' + array[x] + " " + array[x+1] + " " + array[x+2] + " added at index " + x);
-    x++;
-    //document.getElementById("name").value = "";
-};
-
-/*
-function display()
-{
-    for(var y = 0; y < array.length; y++)
-    {
-        e += "Element " + y + " = " + array[y] + "<br>";
-    }
-    document.getElementsById("Result").innerHTML = e;
-    //window.location.href = "view.html";
-    window.location.replace("view.html");
+function myfunction(){
+	var sname = document.getElementById('name').value;
+	var semail = document.getElementById('email').value;
+	var bdate = document.getElementById('dateOfBirth').value;
+	 var person = {
+		name : sname,
+		email : semail,
+		birthdate : bdate
+	};
+	localStorage.setItem('sname',person['name']);
+	localStorage.setItem('semail',person['email']);
+    localStorage.setItem('bdate',person['birthdate']);
 }
-*/
-
-function display() 
-{
-    var e;
-    for(var y = 0; y < array.length; y ++)
-    {
-        e += "Element " + y + " = " + array[y] + "<br>";
-    }
-    //return e;
-    window.location.href = "view.html"  ;
-  
-} 
